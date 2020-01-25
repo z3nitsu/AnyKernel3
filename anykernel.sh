ff516f9cbad36cd928d78cc26402b3118363e0ef
Haikal Khairuddin <z4nyx@outlook.com>
2020-01-25 09:08:57 +0000

    Amethyst

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=LightBorn Kernel by z4nyx
+kernel.string=Amethyst Kernel by z4nyx
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
